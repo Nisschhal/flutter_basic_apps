e8f498ade51f8d2369a6dd579dd79ba9f3e10290
Nisschhal <mrnischalpuri@gmail.com>
2022-09-19 15:46:26 +0545

    stack, positioned, getters, icons

--- flutter_learning/meal_app/lib/widgets/mealItem.dart
+++ flutter_learning/meal_app/lib/widgets/mealItem.dart
@@ -21,6 +21,40 @@ class MealItem extends StatelessWidget {
     required this.affordability,
   });
 
+  String get getComplexity {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get getAffordability {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+
+      default:
+        return 'Unknown';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -29,17 +63,81 @@ class MealItem extends StatelessWidget {
       ),
       elevation: 5,
       margin: const EdgeInsets.all(10),
-      child: Stack(children: [
-        // to maintain the radius of border in image, as by default image cannot maintaint it radius with card's radius
-        ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(15),
-            topRight: Radius.circular(15),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              // to maintain the radius of border in image, as by default image cannot maintaint it radius with card's radius
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Image.network(imageUrl,
+                    height: 250, width: double.infinity, fit: BoxFit.cover),
+              ),
+              Positioned(
+                right: 0,
+                bottom: 10,
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  width: 250,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        bottomLeft: Radius.circular(15)),
+                    color: Colors.black54,
+                  ),
+                  child: Text(
+                    title,
+                    style: const TextStyle(fontSize: 22, color: Colors.white),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+              ),
+            ],
           ),
-          child: Image.network(imageUrl,
-              height: 250, width: double.infinity, fit: BoxFit.cover),
-        )
-      ]),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    child: Row(
+                  children: [
+                    Icon(Icons.schedule),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(duration.toString()),
+                  ],
+                )),
+                Container(
+                    child: Row(
+                  children: [
+                    Icon(Icons.work),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(getComplexity),
+                  ],
+                )),
+                Container(
+                    child: Row(
+                  children: [
+                    Icon(Icons.attach_money),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(getAffordability),
+                  ],
+                )),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
